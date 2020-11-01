f62f8f463fbf53e674499a2c92c559349ef2fa9c
SIMOACR <medachkour@gmail.com>
2020-11-01 17:33:24 +0100

    creating exceptions

---
+++ src/main/java/com/technoWeb/Tp/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.technoWeb.Tp.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
