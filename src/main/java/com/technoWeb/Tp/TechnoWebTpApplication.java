63a5e1f8e5eaf00b05203c5fde1a86f9554c39e6
aek-iliass <iliassaek@gmail.com>
2020-12-20 01:12:25 +0100

    deploy to tomcat servlet container

--- src/main/java/com/technoWeb/Tp/TechnoWebTpApplication.java
+++ src/main/java/com/technoWeb/Tp/TechnoWebTpApplication.java
@@ -2,9 +2,10 @@ package com.technoWeb.Tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TechnoWebTpApplication {
+public class TechnoWebTpApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechnoWebTpApplication.class, args);
