f79e000842436cac1ac55c58f3c9bebef4874fdd
SIMOACR <medachkour@gmail.com>
2020-12-20 06:39:06 +0100

    series adding new methods

--- src/main/java/com/technoWeb/Tp/controller/SeriesController.java
+++ src/main/java/com/technoWeb/Tp/controller/SeriesController.java
@@ -1,6 +1,7 @@
 package com.technoWeb.Tp.controller;
 
 import com.technoWeb.Tp.model.Series;
+import com.technoWeb.Tp.model.SeriesWithUserName;
 import com.technoWeb.Tp.service.series.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,11 @@ public class SeriesController {
         return new ResponseEntity<>(seriesService.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("user/{userName}")
+    public ResponseEntity<List<Series>> getAllByUserName(@PathVariable("userName") String userName) {
+        return new ResponseEntity<>(seriesService.findByUserName(userName), HttpStatus.OK);
+    }
+
     @GetMapping("{id}")
     public ResponseEntity<Series> getById(@PathVariable("id") long id) {
         return new ResponseEntity<>(seriesService.findById(id), HttpStatus.OK);
@@ -32,9 +38,19 @@ public class SeriesController {
         return new ResponseEntity<>(seriesService.create(series), HttpStatus.CREATED);
     }
 
+    @PostMapping("userName")
+    public ResponseEntity<Series> createUsingUserName(@Valid @RequestBody SeriesWithUserName series) {
+        return new ResponseEntity<>(seriesService.createUsingUserName(series), HttpStatus.CREATED);
+    }
+
     @PutMapping
     public ResponseEntity<Series> update(@Valid @RequestBody Series series) {
         return new ResponseEntity<>(seriesService.update(series), HttpStatus.CREATED);
+    }
+
+    @PutMapping("userName")
+    public ResponseEntity<Series> updateUsingUserName(@Valid @RequestBody SeriesWithUserName series) {
+        return new ResponseEntity<>(seriesService.updateWithUserName(series), HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
--- src/main/java/com/technoWeb/Tp/service/series/SeriesService.java
+++ src/main/java/com/technoWeb/Tp/service/series/SeriesService.java
@@ -1,10 +1,8 @@
 package com.technoWeb.Tp.service.series;
 
-import com.technoWeb.Tp.exception.NoContentException;
-import com.technoWeb.Tp.exception.NotFoundException;
-import com.technoWeb.Tp.exception.SeriesErrorMessages;
-import com.technoWeb.Tp.exception.UnauthorizedException;
+import com.technoWeb.Tp.exception.*;
 import com.technoWeb.Tp.model.Series;
+import com.technoWeb.Tp.model.SeriesWithUserName;
 import com.technoWeb.Tp.repository.SeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +20,9 @@ public class SeriesService {
     @Autowired
     private SeriesMapper seriesMapper;
 
+    @Autowired
+    private SeriesWithUserNameMapper seriesWithUserNameMapper;
+
     public List<Series> findAll() {
         List<SeriesEntity> seriesEntities = seriesRepository.findAll();
         if (!seriesEntities.isEmpty())
@@ -32,9 +33,33 @@ public class SeriesService {
             throw new NoContentException(SeriesErrorMessages.NO_SERIES.name());
     }
 
+    public List<Series> findByUserName(String userName) {
+        return seriesRepository.findAllByUserEntityUserName(userName)
+                .orElseThrow(() -> new NoContentException(EventErrorMessages.NO_EVENT.name()))
+                .stream()
+                .map(seriesMapper::toModel).collect(Collectors.toList());
+    }
+
     public Series findById(long id) {
         return seriesRepository.findById(id).map(seriesMapper::toModel)
                 .orElseThrow(() -> new NotFoundException(SeriesErrorMessages.SERIES_NOT_FOUND.name()));
+    }
+
+    public Series createUsingUserName(SeriesWithUserName series) {
+        SeriesEntity seriesEntity = seriesWithUserNameMapper.fromModel(series);
+        long id = series.getId();
+        if(id == 0) {
+            seriesEntity.setId(id);
+            try {
+                return seriesMapper.toModel(seriesRepository.save(seriesEntity));
+            }
+            catch(org.springframework.dao.DataIntegrityViolationException e)
+            {
+                throw new UnauthorizedException(SeriesErrorMessages.SERIES_DUPLICATED_TITLE.name());
+            }
+        } else {
+            throw new UnauthorizedException(SeriesErrorMessages.SERIES_ALREADY_EXIST.name());
+        }
     }
 
     public Series create(Series series) {
@@ -51,6 +76,23 @@ public class SeriesService {
             }
         } else {
             throw new UnauthorizedException(SeriesErrorMessages.SERIES_ALREADY_EXIST.name());
+        }
+    }
+
+    public Series updateWithUserName(SeriesWithUserName series) {
+        SeriesEntity seriesEntity = seriesWithUserNameMapper.fromModel(series);
+        long id = series.getId();
+        if(id != 0) {
+            seriesEntity.setId(id);
+            try {
+                return seriesMapper.toModel(seriesRepository.save(seriesEntity));
+            }
+            catch(org.springframework.dao.DataIntegrityViolationException e)
+            {
+                throw new UnauthorizedException(SeriesErrorMessages.SERIES_DUPLICATED_TITLE.name());
+            }
+        } else {
+            throw new UnauthorizedException(SeriesErrorMessages.NEW_SERIES.name());
         }
     }
 
